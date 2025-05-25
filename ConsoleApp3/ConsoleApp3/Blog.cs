755d4ab1e6f0b3bbbd69c0f5629af5eb44256431
LojainHmda <lojainhmda@gmsil.com>
2025-05-25 23:15:58 +0300

    Add project files.

---
+++ ConsoleApp3/ConsoleApp3/Blog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3.ConsoleApp3
+{
+    internal class Blog
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+
+
+
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp3.ConsoleApp3;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp2.Data
+{
+    internal class ApplicationDpContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=;Database=fakestore;Trusted_conniction=True;TrustServerCertificate=True");
+        }
+    }
+}
